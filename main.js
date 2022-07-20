c3291182556492104c644ca4d3b65282a3b5d406
OwlWhoo <69802847+OwlWhoo@users.noreply.github.com>
2022-07-20 10:35:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function draw(){
         player.xVel -=1;
     }
     player.xVel *= 0.9;
-    if (x<1000||x>1) {
+    if (player.x<1000 && player.x>1) {
       player.x += player.xVel;
     }
   }
